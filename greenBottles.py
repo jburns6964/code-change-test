2597d9ddc6de3b698b61e0012db96d3b3d203016
jburns6964 <jburns6964@sohamcollege.org.uk>
2014-05-07 09:36:02 +0100

    Update greenBottles.py
    i did this with loops

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,12 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+import time
+p = 10
+while p != 0:
+   print("There are {0} green bottles hanging for a wall".format(p))
+   time.sleep(0.2)
+   print("There are {0} green bottles hanging for a wall".format(p))
+   time.sleep(0.2)
+   print("and if 1 green bottle should accidentaly fall")
+   time.sleep(0.2)
+   p = p-1
+   print("there will be {0} green bottles hanging on the wall".format(p))
+   print()
