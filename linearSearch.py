9855b66ee76209aadc2d1463cb0159d70e1dab43
Joshua Burns <jburns6964@sohamcollege.org.uk>
2014-05-11 20:42:14 +0100

    Update linearSearch.py

--- linearSearch.py
+++ linearSearch.py
@@ -3,9 +3,9 @@ searchChar = ""
 charList = []
 lastPosition = -1
 
-
 while testString == "":
     testString = input("Please enter some text to search : ")
+    str.lower(testString)
 
 while len(searchChar) != 1:
     searchChar = input("Enter a character to search for :")
@@ -15,4 +15,20 @@ for x in range (len(testString)):
         charList.append(x-lastPosition)
         lastPosition = x
 
+if searchChar == str.lower(searchChar):
+    searchChar = str.capitalize(searchChar)
+elif searchChar == str.capitalize(searchChar):
+    searchChar = str.lower(searchChar)    
+
+for x in range (len(testString)):
+    if testString[x] == searchChar:
+        charList.append(x-lastPosition)
+        lastPosition = x
+
+if searchChar == str.lower(searchChar):
+    searchChar = str.capitalize(searchChar)
+elif searchChar == str.capitalize(searchChar):
+    searchChar = str.lower(searchChar)   
+
 print ("I found {0} occurences of {1}".format(len(charList),searchChar))
+
